9482a75cf98cf9fbf7e43b85db69085d716edfab
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-15 21:32:38 -0500

    calc finished

--- calculator.js
+++ calculator.js
@@ -1,4 +1,31 @@
-var calculator = {
-  sum: 0,
-  product; 1;
+console.log("sup");
+
+const calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+
+  sub: function(a, b) {
+    return a - b;
+  },
+
+  mult: function(a, b) {
+    return a * b;
+  },
+
+  div: function(a, b) {
+    return a / b;
+  },
+
+  exponent: function(a, b) {
+    return a ** b;
+  },
+
+  fancyMath: function(a, b) {
+    return (a * b) / b;
+  }
 }
+
+console.log(calculator.exponent(2, 3));
+
+
